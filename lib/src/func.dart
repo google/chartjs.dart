b65da5307b249ce3a62b2d9685ce92c8f0da5c30
Abhijeeth Padarthi <rkinabhi@gmail.com>
2019-03-17 22:14:15 +0530

    remove redundant "new" and format code.

--- lib/src/func.dart
+++ lib/src/func.dart
@@ -2,6 +2,6 @@
 // for details. All rights reserved. Use of this source code is governed by a
 // BSD-style license that can be found in the LICENSE file.
 
-typedef R Func0<R>();
-typedef R Func1<A, R>(A a);
-typedef R Func1Opt1<A, R>([A a]);
+typedef Func0<R> = R Function();
+typedef Func1<A, R> = R Function(A a);
+typedef Func1Opt1<A, R> = R Function([A a]);
